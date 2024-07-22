aa6feb35050e6b7db875373673ace3c76924b44b
AFERY_RS <arferyrs@gmail.com>
2024-07-22 12:19:39 +0700

    update_main

--- user/edit_pass.php
+++ user/edit_pass.php
@@ -2,46 +2,12 @@
 require('../db.php');
 session_start();
 
-echo "test";
+if (isset($_SESSION['user_id']) && isset($_SESSION['user_name'])) {
 
-/*   test__________________________1
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
-    exit();
-}
-
-$user_password = $_POST['user_password'];
-$user_new_pass = $_POST['user_new_pass'];
-$user_checkpass = $_POST['user_checkpass']
-
-
-
-
-$stmt = $conn->prepare("SELECT * FROM tb_user WHERE user_password = :user_password");
-$stmt->bindParam(':user_password', $user_password);
-$stmt->execute();
-$userData = $stmt->fetch(PDO::FETCH_ASSOC);
-
-if ($userData) {
-    if ($userData && password_verify($user_password, $userData['user_password'])) {
-        $hashed_password = password_hash($user_new_pass, PASSWORD_DEFAULT);
-
-        $up_db = "UPDATE tb_users SET user_id= ?  WHERE user_id = ?";
-        $up_stmt = $conn->prepare($up_db);
-        $up_stmt->bindParam($hashed_password, $_SESSION['user_id']);
-        $up_stmt->execute();
-
-        echo "เปลี่ยนรหัสผ่านสำเร็จ";
-    } else {
-        echo "รหัสผ่านเดิมไม่ถูกต้อง";
-    }
-}*/
-print_r($_POST);
-//if (isset($_SESSION['user_id']) && isset($_SESSION['user_name'])) {
+    print_r($_POST);
 
     //include "db_conn.php";
 
-    /* 
     if (isset($_POST['user_password']) && isset($_POST['user_new_pass']) && isset($_POST['user_checkpass'])) {
 
         function validate($data)
@@ -87,7 +53,7 @@ print_r($_POST);
                     $hashed_password = password_hash($user_new_pass, PASSWORD_DEFAULT);
 
                     // รหัสผ่านใหม่ในฐานข้อมูล
-                    $stmt_update = $conn->prepare("UPDATE tb_users SET password = :hashed_password WHERE user_id = :user_id");
+                    $stmt_update = $conn->prepare("UPDATE tb_users SET user_password = :hashed_password WHERE user_id = :user_id");
                     //$stmt_update = $pdo->prepare($sql_update);
                     $stmt_update->bindParam(':hashed_password', $hashed_password, PDO::PARAM_STR);
                     $stmt_update->bindParam(':user_id', $user_id, PDO::PARAM_INT);
@@ -102,7 +68,7 @@ print_r($_POST);
                     echo "error=Your current password is incorrect";
                     exit();
                 }
-            } /*else {
+            } else {
                 // ไม่พบข้อมูลผู้ใช้
                 header("Location: ../1page/change_pass.php");
                 echo "error=User not found";
@@ -113,5 +79,5 @@ print_r($_POST);
         // ถ้าไม่มีการส่งข้อมูลแบบ POST
         header("Location: ../1page/change_pass.php");
         exit();
-    } */
-//}
+    }
+}
